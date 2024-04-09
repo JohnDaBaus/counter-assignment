13550d98316a983ce1cd9d57dadd1e6107dc284c
JohnDaBaus <john.dinh.03@hotmail.com>
2024-04-08 22:49:43 -0400

    Unit Test

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,28 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import { render, screen, fireEvent } from '@testing-library/react';
+import Counter from "../components/Counter";
 
 beforeEach(() => {
-  // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  const msg = screen.getByText('Counter');
+  expect(msg).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  const zeroCount = screen.getByTestId("count");
+  expect(zeroCount).toHaveTextContent("0");
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  fireEvent.click(screen.getByText("+"));
+  const inc = screen.getByTestId("count");
+  expect(inc).toHaveTextContent("1");
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  fireEvent.click(screen.getByText("-"));
+  const dec = screen.getByTestId("count");
+  expect(dec).toHaveTextContent("-1");
 });
